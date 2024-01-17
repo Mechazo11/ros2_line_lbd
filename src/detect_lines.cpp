c4afc2589d51501226e3bde03e239fee459b1a70
Mechazo11 <azmyin12@gmail.com>
2024-01-17 12:40:09 -0600

    Working, fully working in ROS 2

--- src/detect_lines.cpp
+++ src/detect_lines.cpp
@@ -37,8 +37,11 @@
 class LineLBD : public rclcpp::Node{
   public:
     
-    // Gobal variables
+    // Gobal variables and settings
     bool useLSDAlgorithm = true;
+    int numOfOctave = 1;
+    float octaveRatio = 2.0;  
+    cv::Mat raw_img; // Variable to hold a raw image
 
     LineLBD():Node("line_lbd"){
       // Constructor
@@ -47,22 +50,43 @@ class LineLBD : public rclcpp::Node{
       std::string packagePath = "ros2_test/src/ros2_line_lbd/"; //! HARDCODED
       std::string pathToTestImage = homeDir + "/" + packagePath + "data/cabinet.png";
 
+      raw_img = cv::imread(pathToTestImage, 1); // Read image in color
 
       // Debug
-      RCLCPP_INFO(this->get_logger(), "pathToTestImage %s", pathToTestImage.c_str());
+      // RCLCPP_INFO(this->get_logger(), "pathToTestImage %s", pathToTestImage.c_str());
+      // Initialization complete
     
     }
 
     ~LineLBD(){
       // Destructor
-      cv::destroyAllWindows(); // Clears all openCV windows
     }
 
-    void runMethod(){
-      pass;
-    }
+    void doLineDetection(){
+      //* Runs once
+      // Declare an object to use the line_lbd_lib library within this package
+      line_lbd_detect* lineLbdPtr = new line_lbd_detect(numOfOctave,octaveRatio); 
+      lineLbdPtr->use_LSD = useLSDAlgorithm;
+      lineLbdPtr->line_length_thres = 15;  // remove short edges
+      
+      
+      //* Code mostly same as Dr. Yang`s code from his ROS1 package
+      cv::Mat out_edges;
+      std::vector< KeyLine> keylines_raw,keylines_out;
+      lineLbdPtr->detect_raw_lines(this->raw_img,keylines_raw);
+      lineLbdPtr->filter_lines(keylines_raw,keylines_out);  // remove short lines
 
-  private:
+      // show image
+      if( this->raw_img.channels() == 1 ){
+        cv::cvtColor(this->raw_img, this->raw_img, cv::COLOR_GRAY2BGR);
+      }
+        
+      cv::Mat raw_img_cp;
+      drawKeylines(this->raw_img, keylines_out, raw_img_cp, cv::Scalar( 0, 150, 0 ),2); //BGR, drawKeylines is part of cv::line_descriptor method
+      cv::imshow( "Line detector", raw_img_cp );
+      cv::waitKey();
+
+    }
 
 };
 
@@ -74,9 +98,10 @@ int main(int argc, char** argv )
 
     // A node declaration
     auto node = std::make_shared<LineLBD>();
-    // Execution function
-
-    // rclcpp::spin(node); // Blocking node
+    node->doLineDetection();
+    rclcpp::spin(node); // Blocking node
+    
+    cv::destroyAllWindows(); // Clears all openCV windows
     rclcpp::shutdown();
     return 0;
   
